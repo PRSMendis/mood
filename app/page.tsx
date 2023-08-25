1a1a75f42ab90662261820c49625b8a7dada3130
PRSMendis <rayhan.mendis@gmail.com>
2023-08-25 11:16:18 +1000

    add clerk for auth

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className='w-full max-w-[600px]'>
         <h1 className='text-6xl'>mood</h1>
-        <p>This is the best app for tracking your mood. All you have tod do is be real.</p>
+        <p>This is the best app for tracking your mood. All you have to do is be real.</p>
         <div>
           <Link href='/journal'>
             <button className='bg-blue-600 px-4 py-2 rounded-lg text-xl'>get started</button>
