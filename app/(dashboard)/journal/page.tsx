223eddddc3681853716ba1864868658b10c997f7
PRSMendis <rayhan.mendis@gmail.com>
2023-08-29 09:43:18 +1000

    fixed router problem, and added newEntryCard logic

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,5 @@
 import EntryCard from '@/components/EntryCard';
-import NewEntryCard from '@/components/NewEntryCard';
+import NewEntryCard from '@/components/newEntryCard';
 import { getUserByClerkID } from '@/utils/auth';
 import {prisma} from '@/utils/db'
 
@@ -20,7 +20,6 @@ const getEntries = async () => {
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  console.log('entries: ', entries);
   return (
     <div className='bg-zinc-400/10 h-full'>
       <h2 className='text-3xl mb-8'>Journal</h2>
--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,9 +1,28 @@
+'use client'
+
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
+
 const NewEntryCard = () => {
+    const router = useRouter()
+    const handleOnClick = async () => {
+        console.log('clicked')
+        const data = await createNewEntry()
+        console.log('data: ', data);
+        // router push adds new url to stack, so that back button works
+        router.push(`/journal/${data.id}`)
+    }
+    const handleOnClick2 = () => {
+        console.log('clicked')
+    }
     return (
-        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg ">
-          <div className="px-4 py-5 sm:px-6">date</div>
-          <div className="px-4 py-5 sm:p-6">entry.analysis.summary</div>
-          <div className="px-4 py-4 sm:px-6">entry.analysis.mood</div>
+        <div
+          className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
+         
+        >
+          <div className="px-4 py-5 sm:p-6"  onClick={handleOnClick}>
+            <span className="text-3xl">New Entry</span>
+          </div>
         </div>
       )
 }
