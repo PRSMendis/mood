88e9916d053cadabac0713c62854844ce11709af
PRSMendis <rayhan.mendis@gmail.com>
2023-08-30 11:12:48 +1000

    add autosave with debounce

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,13 +1,29 @@
 'use client'
+import { updateEntry } from "@/utils/api"
 import { useState } from "react"
+import { useAutosave } from "react-autosave"
 
  
 
 const Editor = ({entry}) => {
   const [text, setText] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useAutosave({
+    data: text,
+    onSave: async (_text) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _text)
+      setIsLoading(false)
+    },
+  })
   return (
     <div className="w-full h-full">
-      <textarea className="w-full h-full p-8 text-xl" value = {text} onChange={(e)=> setText(e.target.value)}/>
+      {isLoading && (<div>...loading</div>)}
+      <textarea 
+        className="w-full h-full p-8 text-xl" 
+        value ={text} 
+        onChange={(e)=> setText(e.target.value)}/>
     </div>
   )
 }
--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,20 @@
 const createURL = (path: string) => {
     return window.location.origin + path
 }
+export const updateEntry = async (id, content) => {
+    const res = await fetch(new Request(createURL(`/api/journal/${id}`), {
+        method: 'PATCH',
+        body: JSON.stringify({content}),
+    }))
+
+    if (res.ok) {
+        const json = await res.json()
+        return json.data
+    }
+
+    //.. when error
+    // return {error: true, code  : 2310, message: 'Something went wrong'}
+}
 
 export const createNewEntry = async () => {
     const res = await fetch(new Request(createURL("/api/journal"), {
