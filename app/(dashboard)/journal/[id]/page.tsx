2a02536766dce47ec84fc2c3a5e285445cec9ec0
PRSMendis <rayhan.mendis@gmail.com>
2023-08-30 10:26:56 +1000

    working text editor

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -18,11 +18,12 @@ const getEntry = async (id)=>  {
 const EntryPage = async ({params}) => {
     // grab the id from the url, and request the appropriate entry from the db
     const entry = await getEntry(params.id)
-    return <div>    
+    return (
+    <div className="w-full h-full">    
         {/* can pass this data from server to client, as entry is serialisable 
         this is because entry was on the database, it woudln't be on the db if it wasn't serialisable */}
         <Editor entry={entry}/>
-        </div>
+    </div>)
 }
 
 export default EntryPage
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,9 +1,13 @@
-'use client' 
+'use client'
+import { useState } from "react"
+
+ 
 
 const Editor = ({entry}) => {
+  const [text, setText] = useState(entry.content)
   return (
-    <div>
-      <h1>{entry.content}</h1>
+    <div className="w-full h-full">
+      <textarea className="w-full h-full p-8 text-xl" value = {text} onChange={(e)=> setText(e.target.value)}/>
     </div>
   )
 }
