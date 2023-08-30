9489811e24464db93c00e7b51787a1536053a217
PRSMendis <rayhan.mendis@gmail.com>
2023-08-30 10:09:14 +1000

    displaying entries on the page

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -6,14 +6,22 @@ const getEntry = async (id)=>  {
     const user = await getUserByClerkID()
     const entry = await prisma.journalEntry.findUnique({
         where: {
-            id
+            userId_id: {
+                userId: user.id,
+                id,
+            }
+
         }
     })
     return entry
 }
-const EntryPage = ({params}) => {
-    return <div>
-        <Editor entry={params}/>
+const EntryPage = async ({params}) => {
+    // grab the id from the url, and request the appropriate entry from the db
+    const entry = await getEntry(params.id)
+    return <div>    
+        {/* can pass this data from server to client, as entry is serialisable 
+        this is because entry was on the database, it woudln't be on the db if it wasn't serialisable */}
+        <Editor entry={entry}/>
         </div>
 }
 
