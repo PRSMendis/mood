102d8483bc637aac4eb8987d30c55b2f917ca41e
PRSMendis <rayhan.mendis@gmail.com>
2023-08-30 10:00:48 +1000

    db changes for unique JournalEntry

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,20 @@
+import Editor from "@/components/Editor"
+import { getUserByClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getEntry = async (id)=>  {
+    const user = await getUserByClerkID()
+    const entry = await prisma.journalEntry.findUnique({
+        where: {
+            id
+        }
+    })
+    return entry
+}
 const EntryPage = ({params}) => {
-    return <div>{params.id}</div>
+    return <div>
+        <Editor entry={params}/>
+        </div>
 }
 
 export default EntryPage
