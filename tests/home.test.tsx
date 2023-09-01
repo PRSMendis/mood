86e5f3df1764751d0b3e2e2ef8b8f64fda01a2bd
PRSMendis <rayhan.mendis@gmail.com>
2023-09-01 14:49:17 +1000

    added test for homePage

---
+++ tests/home.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import Home from '../app/page'
+
+vi.mock('@clerk/nextjs', () => {
+    const mockedFunctions = {
+        auth: () => new Promise((resolve) => resolve({userId: '123'})),
+        ClerkProvider: ({children}: any) => <div>{children}</div>,
+        useUser: () => ({
+            isSignedIn: true,
+            user: {
+              id: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC',
+              fullName: 'Charles Harris',
+            },
+        }),
+    }
+    return mockedFunctions
+})
+
+test('Home', async () => {
+    render (await Home())
+    expect(screen.getByText('get started')).toBeTruthy()
+})
